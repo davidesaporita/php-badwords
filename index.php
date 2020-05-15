fd1db8c63bdfae4538d80c213d38e9dd62c017da
Davide Saporita <davide.saporita@gmail.com>
2020-05-15 16:25:16 +0200

    Completed

--- index.php
+++ index.php
@@ -16,6 +16,30 @@
 </head>
 <body>
     
+    <?php  
     
+    $par = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quis in, perspiciatis cupiditate inventore dolore similique consequatur eaque dolores quisquam quas? Magnam sunt dolorum exercitationem asperiores! Perferendis perspiciatis corrupti accusantium!";
+    $par_length = strlen($par);
+
+    if($_GET['check']) {
+        $word_to_replace = $_GET['check'];
+        $par_updated = str_ireplace($word_to_replace,'***',$par);
+    }
+
+    ?>
+    <header>
+        <h1>PHP Bad Words</h1>
+    </header>
+
+    <main>
+        <div>
+            <p><?php echo $par_updated ? $par_updated : $par; ?></p>
+            <p>Lunghezza paragrafo: <?php echo $par_length; ?> mila chilometri</p>
+        </div>
+    </main>
+    <footer>
+        <i>Made by produzione orticola®</i>
+    </footer>
+
 </body>
 </html>
