1d27f33a38752615e60fd09eb16ae9d4aad7f9c2
Davide Saporita <davide.saporita@gmail.com>
2020-05-15 16:04:50 +0200

    Added project files

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!--
+    PHP Badwords
+    Creare una variabile con un paragrafo di testo.
+    Visualizzare a schermo il paragrafo
+    Visualizzare a schermo la relativa lunghezza del paragrafo
+    Sostituire tutte le ricorrenze, nel paragrafo creato, della badword passata in GET (query string) con tre * (asterschi). 
+    -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ehi man<, don't say bad words</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
